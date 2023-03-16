95ac6f67c3fe7e54d88651a5059114976b42818c
nicalebis <98501090+nicalebis@users.noreply.github.com>
2023-03-16 18:04:02 -0400

    Update data_manager.js

--- data_manager.js
+++ data_manager.js
@@ -3,19 +3,39 @@ class DataManager {
     this.data = [];
   }
 
-  addData(trialId, eventType, time) {
+  addData(trialId, eventType, time, videoTime, relativeTime) {
     this.data.push({
       trialId: trialId,
       eventType: eventType,
       time: time,
+      videoTime: videoTime,
+      relativeTime: relativeTime,
     });
   }
 
   sendDataToServer() {
-    // Send the data to your server
-    // You can use XMLHttpRequest, Fetch API, or any other method to send the data
-    console.log("Sending data to server:", this.data);
+    // Convert data to CSV format and download it
+    const csvData = convertToCSV(this.data);
+    const blob = new Blob([csvData], { type: "text/csv" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "experiment_data.csv";
+    link.click();
+    URL.revokeObjectURL(url);
+    console.log("CSV file downloaded:", this.data);
   }
+}
+
+function convertToCSV(data) {
+  const header = ["Trial ID", "Event", "Timestamp", "Video Time", "Relative Time"];
+  const csvRows = [header.join(",")];
+
+  for (const row of data) {
+    csvRows.push(Object.values(row).join(","));
+  }
+
+  return csvRows.join("\n");
 }
 
 const dataManager = new DataManager();
