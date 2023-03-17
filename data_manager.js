726b674a72993f305b617bfcefed23c5da6025b2
nicalebis <98501090+nicalebis@users.noreply.github.com>
2023-03-17 00:50:00 -0400

    Update data_manager.js

--- data_manager.js
+++ data_manager.js
@@ -26,29 +26,3 @@ class DataManager {
 
 const dataManager = new DataManager();
 
-// sendDataToServer() {
-//     // Convert data to CSV format and download it
-//     const csvData = convertToCSV(this.data);
-//     const blob = new Blob([csvData], { type: "text/csv" });
-//     const url = URL.createObjectURL(blob);
-//     const link = document.createElement("a");
-//     link.href = url;
-//     link.download = "experiment_data.csv";
-//     link.click();
-//     URL.revokeObjectURL(url);
-//     console.log("CSV file downloaded:", this.data);
-//   }
-// }
-
-// function convertToCSV(data) {
-//   const header = ["Trial ID", "Event", "Timestamp", "Video Time", "Relative Time"];
-//   const csvRows = [header.join(",")];
-// 
-//   for (const row of data) {
-//     csvRows.push(Object.values(row).join(","));
-//   }
-// 
-//   return csvRows.join("\n");
-// }
-// 
-// const dataManager = new DataManager();
