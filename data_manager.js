6309df276f5bfad1ccc8ca88ec698000c7714fe2
nicalebis <98501090+nicalebis@users.noreply.github.com>
2023-03-16 21:33:43 -0400

    Update data_manager.js

--- data_manager.js
+++ data_manager.js
@@ -13,29 +13,42 @@ class DataManager {
     });
   }
 
-  sendDataToServer() {
-    // Convert data to CSV format and download it
-    const csvData = convertToCSV(this.data);
-    const blob = new Blob([csvData], { type: "text/csv" });
+  exportData() {
+    const json = JSON.stringify(this.data);
+    const blob = new Blob([json], { type: 'application/json' });
     const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = "experiment_data.csv";
-    link.click();
-    URL.revokeObjectURL(url);
-    console.log("CSV file downloaded:", this.data);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'data.json';
+    a.click();
   }
-}
-
-function convertToCSV(data) {
-  const header = ["Trial ID", "Event", "Timestamp", "Video Time", "Relative Time"];
-  const csvRows = [header.join(",")];
-
-  for (const row of data) {
-    csvRows.push(Object.values(row).join(","));
-  }
-
-  return csvRows.join("\n");
 }
 
 const dataManager = new DataManager();
+
+// sendDataToServer() {
+//     // Convert data to CSV format and download it
+//     const csvData = convertToCSV(this.data);
+//     const blob = new Blob([csvData], { type: "text/csv" });
+//     const url = URL.createObjectURL(blob);
+//     const link = document.createElement("a");
+//     link.href = url;
+//     link.download = "experiment_data.csv";
+//     link.click();
+//     URL.revokeObjectURL(url);
+//     console.log("CSV file downloaded:", this.data);
+//   }
+// }
+
+// function convertToCSV(data) {
+//   const header = ["Trial ID", "Event", "Timestamp", "Video Time", "Relative Time"];
+//   const csvRows = [header.join(",")];
+// 
+//   for (const row of data) {
+//     csvRows.push(Object.values(row).join(","));
+//   }
+// 
+//   return csvRows.join("\n");
+// }
+// 
+// const dataManager = new DataManager();
