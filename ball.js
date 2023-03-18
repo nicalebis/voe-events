57b98eee87f16dd6c3a0b2e7975dff674796ab4a
nicalebis <98501090+nicalebis@users.noreply.github.com>
2023-03-18 00:50:40 -0400

    Update ball.js

--- ball.js
+++ ball.js
@@ -1,18 +1,20 @@
 // BALL ANIMATIONS CODE //
 
 class Ball {
-  constructor(x, y, radius, dx, dy, color) {
+  constructor(x, y, radius, dx, dy, color, context) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.dx = dx;
     this.dy = dy;
     this.color = color;
+    this.context = context;
   }
 
   draw() {
+    const ctx = this.context;
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     ctx.fillStyle = this.color;
     ctx.fill();
     ctx.closePath();
