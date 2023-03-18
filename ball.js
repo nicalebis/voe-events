cf67b0dba73d1d1ef6f73b3fdd927c8e914a005f
nicalebis <98501090+nicalebis@users.noreply.github.com>
2023-03-18 03:52:14 -0400

    Update ball.js

--- ball.js
+++ ball.js
@@ -17,15 +17,16 @@ class Ball {
   }
 
   move(canvas) {
-  this.x += this.dx;
-  this.y += this.dy;
+    if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
 
-  if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
-    this.dx = -this.dx;
-  }
+    if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
 
-  if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
-    this.dy = -this.dy;
+    this.x += this.dx;
+    this.y += this.dy;
   }
 
   update(canvas) {
