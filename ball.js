dfdcdf49462438b063e9a739719da3d7401cadc8
nicalebis <98501090+nicalebis@users.noreply.github.com>
2023-03-18 00:57:36 -0400

    Update ball.js

--- ball.js
+++ ball.js
@@ -19,41 +19,20 @@ class Ball {
   }
 
   update() {
-    this.x += this.dx;
-    this.y += this.dy;
-
-    if (this.x - this.radius < 0 || this.x + this.radius > canvas.width) {
+    if (this.x + this.radius > 640 || this.x - this.radius < 0) {
       this.dx = -this.dx;
     }
 
-    if (this.y - this.radius < 0 || this.y + this.radius > canvas.height) {
+    if (this.y + this.radius > 360 || this.y - this.radius < 0) {
       this.dy = -this.dy;
     }
+
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+
+  animate(ctx) {
+    this.update();
+    this.draw(ctx);
   }
 }
-
-function detectCollision(ball1, ball2) {
-  const dx = ball1.x - ball2.x;
-  const dy = ball1.y - ball2.y;
-  const distance = Math.sqrt(dx * dx + dy * dy);
-  return distance < ball1.radius + ball2.radius;
-}
-
-const ball1 = new Ball(100, 100, 20, 2, 3, "red");
-const ball2 = new Ball(200, 200, 20, -2, -3, "blue");
-const ball3 = new Ball(300, 150, 20, 3, 2, "green");
-const balls = [ball1, ball2, ball3];
-
-function animate(ctx) {
-  ctx.clearRect(0, 0, innerWidth, innerHeight);
-
-  ballArray.forEach((ball) => {
-    ball.update(ctx);
-    ball.draw(ctx);
-  });
-
-  requestAnimationFrame(() => animate(ctx));
-}
-
-init();
-animate();
